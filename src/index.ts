ef8a75eff1280b8355faa91313fa938c212847f8
Talal <talalahmad257@gmail.com>
2022-12-11 15:05:27 +0000

    Throw error if guild isn't found

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import "dotenv/config";
 import { Client, IntentsBitField } from "discord.js";
 import messageHandler from "./handlers/message.js";
-import interactionHandler from "./handlers/interaction.js";
-import { registeredChatInputCommands } from "./handlers/interaction.js";
+import interactionHandler, { registeredChatInputCommands } from "./handlers/interaction.js";
 import mongoose, { connect } from "mongoose";
-const { TOKEN, GUILD_ID, MONGO_URI } = process.env;
+import SettingsModel from "./database/settings.js";
 
+const { TOKEN, GUILD_ID, MONGO_URI, SESSION_TOKEN } = process.env;
 checkEnv();
 
 const client = new Client({
@@ -14,6 +14,8 @@ const client = new Client({
 
 client.on("ready", async client => {
   console.log(`Logged in as ${client.user.tag}!`);
+  const guild = client.guilds.cache.get(GUILD_ID!);
+  if (!guild) throw new Error("Guild not found");
 
   mongoose.set("strictQuery", false);
   await connect(MONGO_URI!)
@@ -31,7 +33,6 @@ client.on("ready", async client => {
     dmPermission: command.dmPermission
   }));
 
-  const guild = client.guilds.cache.get(GUILD_ID!);
   guild?.commands
     .set(commands)
     .then(c =>
@@ -51,5 +52,6 @@ function checkEnv() {
   if (!TOKEN) throw new Error("TOKEN is not defined in .env file");
   if (!GUILD_ID) throw new Error("GUILD_ID is not defined in .env file");
   if (!MONGO_URI) throw new Error("MONGO_URI is not defined in .env file");
+  if (!SESSION_TOKEN) throw new Error("SESSION_TOKEN is not defined in .env file");
 }
 
