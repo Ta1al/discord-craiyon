a6f386c5dce6dd63897e43868e572cb929962b4f
Talal <talalahmad257@gmail.com>
2022-12-11 12:09:04 +0000

    Add ping command

---
+++ src/commands/chatInput/ping.ts
@@ -0,0 +1,12 @@
+import { ChatInputCommand } from "../../handlers/interaction.js";
+
+
+const command: ChatInputCommand = {
+  name: "ping",
+  description: "Replies with pong!",
+  execute: async interaction => {
+    await interaction.reply("Pong!");
+  }
+};
+
+export default command;
