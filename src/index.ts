8fc01b54da38173bf7e5bf8b16831e2ead3e964a
Talal <talalahmad257@gmail.com>
2022-12-11 12:09:34 +0000

    Register commands after 'ready' event

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,33 @@
 import "dotenv/config";
 import { Client, IntentsBitField } from "discord.js";
-import messageHandler from "./handlers/message";
-import interactionHandler from "./handlers/interaction";
-
-const { TOKEN } = process.env;
+import messageHandler from "./handlers/message.js";
+import interactionHandler from "./handlers/interaction.js";
+import { registeredChatInputCommands } from "./handlers/interaction.js";
+const { TOKEN, GUILD_ID } = process.env;
 
 const client = new Client({
   intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages]
 });
 
-client.on("ready", client => console.log(`Logged in as ${client.user?.tag}!`));
+client.on("ready", async client => {
+  console.log(`Logged in as ${client.user.tag}!`);
+
+  const guild = client.guilds.cache.get(GUILD_ID!);
+  const commands = Array.from(registeredChatInputCommands).map(([name, command]) => ({
+    name,
+    description: command.description,
+    options: command.options,
+    defaultPermission: command.defaultMemberPermissions,
+    dmPermission: command.dmPermission
+  }));
+
+  (guild ?? client.application).commands
+    .set(commands)
+    .then(c =>
+      console.log(`Registered ${c.size} command(s) ${guild ? `for guild ${guild.name}` : "globally"}`)
+    )
+    .catch(console.error);
+});
 
 client.on("messageCreate", messageHandler);
 client.on("interactionCreate", interactionHandler);
