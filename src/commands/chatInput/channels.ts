9b5bf11c437820d9028c1b7013c7b2dee5a050c1
Ta1al <talalahmad257@gmail.com>
2022-12-11 15:48:23 +0000

    Add selectmenu

--- src/commands/chatInput/channels.ts
+++ src/commands/chatInput/channels.ts
@@ -1,11 +1,11 @@
 import {
   ApplicationCommandOptionType,
   ChatInputCommandInteraction,
-  GuildChannel,
   Snowflake,
-  TextChannel
+  StringSelectMenuBuilder,
+  TextBasedChannel
 } from "discord.js";
-import { ChatInputCommand } from "../../handlers/interaction.js";
+import { ChatInputCommand, registeredStringSelectComponents } from "../../handlers/interaction.js";
 import SettingsModel, { Settings } from "../../database/settings.js";
 
 const command: ChatInputCommand = {
@@ -21,7 +21,8 @@ const command: ChatInputCommand = {
   dmPermission: false,
   permissionCheck: "owner",
   execute: async interaction => {
-    const channel = interaction.options.getChannel("channel") as TextChannel;
+    const channel = interaction.options.getChannel("channel") as TextBasedChannel;
+    await interaction.deferReply({ ephemeral: true });
     let settings = await SettingsModel.findOne({ guildId: interaction.guildId });
     if (!settings) settings = await SettingsModel.create({ guildId: interaction.guildId });
 
@@ -33,38 +34,60 @@ const command: ChatInputCommand = {
 export default command;
 
 async function listChannels(interaction: ChatInputCommandInteraction, channels: Snowflake[]) {
+  if (!channels.length) return respond(interaction, "❌ No channels are whitelisted")
   const channelList = channels.map((id, i) => `\`${i + 1}.\` <#${id}>`).join("\n");
-  await interaction.reply({
-    content: `Whitelisted Channels:\n${channelList || "None"}`,
-    ephemeral: true
-  });
+  const selectMenu = new StringSelectMenuBuilder()
+    .setCustomId(interaction.id)
+    .setPlaceholder("Select channels to remove")
+    .setMinValues(1)
+    .setMaxValues(25)
+    .addOptions(channels.map((id, i) => ({ label: `${i + 1}. ${id}`, value: id })));
+
+  return void interaction.editReply({
+    content: `Whitelisted Channels:\n${channelList}`,
+    components: [{ type: 1, components: [selectMenu] }],
+  }).then(() => registerComponent(interaction));
 }
 
 async function addChannel(
   interaction: ChatInputCommandInteraction,
   settings: Settings,
-  channel: GuildChannel
+  channel: TextBasedChannel
 ) {
   if (!channel.isTextBased())
-    return void interaction.reply({
-      content: "❌ That channel is not a text channel",
-      ephemeral: true
-    });
-  if (settings.channels.includes(channel.id)) {
-    await interaction.reply({
-      content: "⚠ That channel is already whitelisted",
-      ephemeral: true
-    });
-    return;
-  }
+    return void respond(interaction, "❌ That channel is not a text channel");
+  if (settings.channels.includes(channel.id))
+    return void respond(interaction, "⚠ That channel is already whitelisted");
+  if (settings.channels.length === 25)
+    return void respond(interaction, "❌ You can't have more than 25 whitelisted channels");
+
 
   await SettingsModel.updateOne(
     { guildId: interaction.guildId },
     { $push: { channels: channel.id } }
   );
-  await interaction.reply({
-    content: `Added ${channel.toString()} to the whitelist`,
-    ephemeral: true
-  });
+  return void respond(interaction, "✅ Channel added");
 }
 
+async function respond(interaction: ChatInputCommandInteraction, content: string) {
+  await interaction.editReply({ content });
+}
+
+function registerComponent(interaction: ChatInputCommandInteraction) {
+  registeredStringSelectComponents.set(interaction.id, {
+    allowedUsers: [interaction.user.id],
+    execute: async selectInteraction => {
+      const { values } = selectInteraction;
+      await selectInteraction.deferUpdate();
+      await SettingsModel.updateOne(
+        { guildId: interaction.guildId },
+        { $pull: { channels: { $in: values } } }
+      );
+
+      return void selectInteraction.update({
+        content: `✅ Removed ${values.length} channel${values.length === 1 ? "" : "s"}`,
+        components: []
+      })
+    }
+  });
+}
