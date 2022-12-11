71cce828e25527e2490131be87631266d8145349
Talal <talalahmad257@gmail.com>
2022-12-11 12:08:52 +0000

    Setup message handler

--- src/handlers/message.ts
+++ src/handlers/message.ts
@@ -1,5 +1,33 @@
-import { Awaitable, Message } from "discord.js";
+import { Message } from "discord.js";
+import { readdir } from "fs/promises";
+
+export const registeredMessageCommands = new Map<string, MessageCommand>();
+const commands = await readdir("./src/commands/message", { withFileTypes: false });
+for (const command of commands) {
+  const { default: MessageCommand } = await import(
+    `../commands/message/${command.split(".ts")[0]}.js`
+  );
+  registeredMessageCommands.set(MessageCommand.name, MessageCommand);
+}
 
 export default async function messageHandler(message: Message): Promise<void> {
-  return void await message.reply("Hello, world!");
+  if (message.author.bot) return;
+  try {
+    if (message.mentions.has(message.client.user)) {
+      message.content = removeMention(message);
+      return registeredMessageCommands.get("mention")?.execute(message);
+    }
+  } catch (error) {
+    console.error(error);
+  }
 }
+
+export interface MessageCommand {
+  name: string;
+  execute: (message: Message) => Promise<void>;
+}
+
+function removeMention(message: Message<boolean>): string {
+  return message.content.replace("<@" + message.client.user.id + ">", "").trim();
+}
+
