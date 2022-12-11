2b171ad4ef9e60f779f7d013868456946824cb47
Talal <talalahmad257@gmail.com>
2022-12-11 12:02:15 +0000

    Setup interaction handler

--- src/handlers/interaction.ts
+++ src/handlers/interaction.ts
@@ -1,7 +1,46 @@
-import { Interaction } from "discord.js";
+import {
+  Interaction,
+  ApplicationCommandOptionData,
+  PermissionResolvable,
+  ChatInputCommandInteraction,
+  Snowflake,
+  ButtonInteraction
+} from "discord.js";
+import { readdir } from "fs/promises";
 
-export default async function interactionHandler(interaction: Interaction): Promise<void> {
-  if (!interaction.isChatInputCommand()) return;
-  return void (await interaction.reply("Hello, world!"));
+export const registeredChatInputCommands = new Map<string, ChatInputCommand>();
+export const registeredButtonComponents = new Map<string, ButtonComponent>();
+
+const commands = await readdir("./src/commands/chatInput");
+for (const command of commands) {
+  const { default: ChatInputCommand } = await import(
+    `../commands/chatInput/${command.split(".ts")[0]}.js`
+  );
+  registeredChatInputCommands.set(ChatInputCommand.name, ChatInputCommand);
+}
+
+export default function interactionHandler(interaction: Interaction): void {
+  try {
+    if (interaction.isChatInputCommand())
+      return void registeredChatInputCommands.get(interaction.commandName)?.execute(interaction);
+    if (interaction.isButton())
+      return void registeredButtonComponents.get(interaction.customId)?.execute(interaction);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export interface ChatInputCommand {
+  name: string;
+  description: string;
+  options?: ApplicationCommandOptionData[];
+  defaultMemberPermissions?: PermissionResolvable;
+  dmPermission?: boolean;
+  execute: (interaction: ChatInputCommandInteraction) => Promise<void>;
+}
+
+export interface ButtonComponent {
+  allowedUsers?: "all" | Snowflake[];
+  execute: (interaction: ButtonInteraction) => Promise<void>;
 }
 
